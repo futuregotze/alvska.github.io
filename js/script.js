7643dafdffe70b44dea3d573f78d5019cd66e66f
futuregotze <33991663+futuregotze@users.noreply.github.com>
2023-11-10 11:34:27 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+function toggleMenu() {
+  var navMenu = document.querySelector('.nav-menu');
+  navMenu.style.display = (navMenu.style.display === 'block') ? 'none' : 'block';
+}
