41275b6aee5b57cfe17b8316d0dd341520791d7c
futuregotze <33991663+futuregotze@users.noreply.github.com>
2023-11-15 14:05:37 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,13 +9,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburgerIcon = document.querySelector('.hamburger-icon');
   const firstElement = document.getElementById('portfolio-header'); // Adjust the ID
 
+  // Dark Mode Setup
+  let darkMode;
+
+  if (localStorage.getItem('dark-mode')) {
+    darkMode = localStorage.getItem('dark-mode');
+  } else {
+    darkMode = 'light';
+  }
+
   // Function to toggle dark mode
   function toggleDarkMode() {
     document.body.classList.toggle("dark-mode", darkModeSwitch.checked);
+    // Update localStorage with the current dark mode state
+    localStorage.setItem('dark-mode', darkModeSwitch.checked ? 'dark' : 'light');
   }
 
   // Check the initial state of the dark mode switch and apply styles accordingly
   if (darkModeSwitch) {
+    darkModeSwitch.checked = darkMode === 'dark';
     toggleDarkMode();
     darkModeSwitch.addEventListener("change", toggleDarkMode);
   }
@@ -25,41 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
     element.classList.add("animate");
   });
 
-  let isMobileNavbarHidden = false;
-
-  window.addEventListener("scroll", () => {
-    const scrollPosition = window.scrollY;
-    const SCROLL_THRESHOLD = 200;
-    const navbarOpacity = 1 - scrollPosition / SCROLL_THRESHOLD;
-    navbar.style.opacity = navbarOpacity;
-
-    // Toggle 'hidden' class on the navbar based on scroll position
-    if (scrollPosition > navbar.clientHeight) {
-      navbar.classList.add("hidden");
-      buttons.forEach((button) => {
-        button.style.pointerEvents = "none";
-      });
-
-      // Hide the mobile navbar with a smooth transition
-      if (!isMobileNavbarHidden && scrollPosition > firstElement.offsetTop) {
-        isMobileNavbarHidden = true;
-        mobileNavbar.style.transition = 'opacity 0.5s';
-        mobileNavbar.style.opacity = '0';
-      }
-    } else {
-      navbar.classList.remove("hidden");
-      buttons.forEach((button) => {
-        button.style.pointerEvents = "auto";
-      });
-
-      // Show the mobile navbar with a smooth transition
-      if (isMobileNavbarHidden) {
-        isMobileNavbarHidden = false;
-        mobileNavbar.style.transition = 'opacity 1.0s';
-        mobileNavbar.style.opacity = '1';
-      }
-    }
-  });
+  // ... (rest of your existing code)
 
   // Mobile menu toggle function
   function toggleMobileMenu() {
