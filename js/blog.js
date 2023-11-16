3dd9893afb0ed4adf7f568898917dd1b38fbcbb8
futuregotze <33991663+futuregotze@users.noreply.github.com>
2023-11-16 00:26:41 -0700

    Create blog.js

---
+++ js/blog.js
@@ -0,0 +1,11 @@
+// Add this file to handle blog post-related logic
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to fetch and display blog posts
+    function fetchAndDisplayBlogPosts() {
+        // Your logic to fetch blog post data (e.g., using fetch API)
+        // Once you have the data, update the HTML to display blog posts in the carousel
+    }
+
+    // Call the function to display blog posts when the page loads
+    fetchAndDisplayBlogPosts();
+});
