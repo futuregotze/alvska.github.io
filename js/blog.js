9245e12c077765948774340325d16fa031eb808d
futuregotze <33991663+futuregotze@users.noreply.github.com>
2023-11-16 01:06:09 -0700

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,53 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Function to fetch and display blog posts
-    function fetchAndDisplayBlogPosts() {
-        // Assuming you have posts from post1.json to post5.json
-        const postPromises = Array.from({ length: 5 }, (_, index) => {
-            const postNumber = index + 1;
-            return fetch(`doc-posts/post${postNumber}.json`)
-                .then(response => response.json());
+  // Function to fetch and display blog posts
+  function fetchAndDisplayPosts() {
+    // Assuming you have an array of post filenames
+    const postFilenames = ["post1.json", "post2.json", "post3.json", "post4.json"];
+
+    // Reference to the carousel container
+    const carouselContainer = document.getElementById("other-posts-carousel");
+
+    // Function to fetch the full content of a post
+    function fetchPostContent(filename) {
+      return fetch(`doc-posts/${filename}`)
+        .then((response) => response.json())
+        .then((post) => {
+          return `
+            <h2>${post.name}</h2>
+            <p class="date">Published on: ${post.date}</p>
+            <img src="${post.image}" alt="${post.name} Image">
+            <p class="tags">Tags: ${post.tags.join(", ")}</p>
+            <div class="full-content">${post.content}</div>
+          `;
+        })
+        .catch((error) => {
+          console.error("Error fetching post:", error);
+          return "Error loading post content.";
         });
-
-        Promise.all(postPromises)
-            .then(posts => {
-                // Assuming data is an array of blog posts
-                // Filter the posts to find the featured post
-                const featuredPost = posts.find(post => post.featured === true);
-
-                if (featuredPost) {
-                    // Display the featured post
-                    const featuredPostSection = document.querySelector('.featured-post');
-                    const featuredPostHTML = `
-                        <div class="post-card">
-                            <h2>${featuredPost.title}</h2>
-                            <p>${featuredPost.content}</p>
-                            <!-- Add more content as needed -->
-                        </div>
-                    `;
-                    featuredPostSection.innerHTML = featuredPostHTML;
-                }
-
-                // Display other posts in a horizontal carousel
-                const otherPostsContainer = document.querySelector('.other-posts .carousel-container');
-                const otherPosts = posts.filter(post => post.featured !== true);
-
-                // You may want to loop through multiple posts and create HTML dynamically
-                const otherPostsHTML = otherPosts.map(post => `
-                    <div class="post-card">
-                        <h2>${post.title}</h2>
-                        <p>${post.content}</p>
-                        <!-- Add more content as needed -->
-                    </div>
-                `).join('');
-
-                otherPostsContainer.innerHTML = otherPostsHTML;
-            })
-            .catch(error => console.error('Error fetching blog posts:', error));
     }
 
-    // Initial call to fetch and display blog posts when the page loads
-    fetchAndDisplayBlogPosts();
+    // Fetch and display each post
+    postFilenames.forEach((filename) => {
+      fetch(`doc-posts/${filename}`)
+        .then((response) => response.json())
+        .then((post) => {
+          // Create a post card
+          const postCard = document.createElement("div");
+          postCard.classList.add("post-card");
 
-    // Set up periodic refresh (every 5 minutes)
-    setInterval(fetchAndDisplayBlogPosts, 5 * 60 * 1000);
+          // Populate the post card with data from the JSON
+          postCard.innerHTML = `
+            <h2>${post.name}</h2>
+            <p class="date">Published on: ${post.date}</p>
+            <img src="${post.image}" alt="${post.name} Image">
+            <p class="tags">Tags: ${post.tags.join(", ")}</p>
+            <!-- Add a link to the full post content -->
+            <a href="#" class="read-more-link" onclick="loadPostContent('${filename}')">Read More</a>
+          `;
+
+          // Append the post card to the carousel container
+          carouselContainer.appendChild(postCard);
+        })
+        .catch((error) => console.error("Error fetching post:", error));
+    });
+
+    // Function to load and display the full content of a post
+    window.loadPostContent = async function (filename) {
+      const fullContent = await fetchPostContent(filename);
+      // Display the full content in a modal or any desired way
+      alert(fullContent);
+    };
+  }
+
+  // Call the function to fetch and display posts
+  fetchAndDisplayPosts();
 });
