9bb6a2209610f6e06e996907096968895d724c44
futuregotze <33991663+futuregotze@users.noreply.github.com>
2023-11-16 09:51:57 -0700

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,69 +1,66 @@
 (function () {
-  document.addEventListener("DOMContentLoaded", function () {
-    const featuredPostContainer = document.querySelector(".featured-post .featured-content");
-    const carouselContainer = document.getElementById("other-posts-carousel");
-    const loadMoreButton = document.getElementById("load-more-button");
+    document.addEventListener("DOMContentLoaded", function () {
+        const featuredPostContainer = document.querySelector(".featured-post .featured-content");
+        const carouselContainer = document.getElementById("other-posts-carousel");
+        const loadMoreButton = document.getElementById("load-more-button");
 
-    // Number of posts to load at a time
-    const postsPerPage = 5;
-    let currentPage = 1;
+        // Number of posts to load at a time
+        const postsPerPage = 5;
+        let currentPage = 1;
 
-    function displayPost(container, content) {
-      const postCard = document.createElement("div");
-      postCard.classList.add("post-card");
-      postCard.innerHTML = content;
+        function displayPost(container, content) {
+            const postCard = document.createElement("div");
+            postCard.classList.add("post-card");
+            postCard.innerHTML = content;
 
-      container.appendChild(postCard);
-    }
-
-    async function loadMorePosts(start, end) {
-      for (let i = start; i <= end; i++) {
-        try {
-          const postContent = await fetchPost(`./doc-posts/post${i}.html`);
-          displayPost(carouselContainer, postContent);
-        } catch (error) {
-          console.error("Error loading post:", error);
+            container.appendChild(postCard);
         }
-      }
-    }
 
-    // Function to fetch and display blog posts
-    function fetchBlogPosts() {
-      // Fetch featured post HTML
-      fetchPost("./doc-posts/featured-post.html")
-        .then((featuredPostContent) => {
-          // Display the featured post
-          displayPost(featuredPostContainer, featuredPostContent);
-        })
-        .catch((error) => {
-          console.error("Error loading featured post:", error);
-        });
+        function loadMorePosts() {
+            // Use a fetch function to load posts dynamically
+            fetchPost(`./doc-posts/post${currentPage}.html`)
+                .then((postContent) => {
+                    displayPost(carouselContainer, postContent);
+                    currentPage++;
+                })
+                .catch((error) => {
+                    console.error("Error loading post:", error);
+                });
+        }
 
-      // Load more posts when the button is clicked
-      loadMoreButton.addEventListener("click", () => {
-        const start = currentPage * postsPerPage + 1;
-        const end = (currentPage + 1) * postsPerPage;
-        loadMorePosts(start, end);
-        currentPage++;
-      });
+        // Function to fetch and display blog posts
+        function fetchBlogPosts() {
+            // Fetch featured post HTML
+            fetchPost("./doc-posts/featured-post.html")
+                .then((featuredPostContent) => {
+                    // Display the featured post
+                    displayPost(featuredPostContainer, featuredPostContent);
+                })
+                .catch((error) => {
+                    console.error("Error loading featured post:", error);
+                });
 
-      // Initial load of posts
-      loadMorePosts(1, postsPerPage);
-      currentPage++;
-    }
+            // Load more posts when the button is clicked
+            if (loadMoreButton) {
+                loadMoreButton.addEventListener("click", loadMorePosts);
+            }
 
-    // Function to fetch a single post from an HTML file
-    async function fetchPost(url) {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Failed to fetch post: ${response.status} ${response.statusText}`);
-      }
+            // Initial load of posts
+            loadMorePosts();
+        }
 
-      const postContent = await response.text();
-      return postContent;
-    }
+        // Function to fetch a single post from an HTML file
+        async function fetchPost(url) {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch post: ${response.status} ${response.statusText}`);
+            }
 
-    // Call the function to fetch and display blog posts
-    fetchBlogPosts();
-  });
+            const postContent = await response.text();
+            return postContent;
+        }
+
+        // Call the function to fetch and display blog posts
+        fetchBlogPosts();
+    });
 })();
