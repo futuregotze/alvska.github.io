fb4dec830e5b06c72bd702e2e0cce125eec662ee
futuregotze <33991663+futuregotze@users.noreply.github.com>
2023-11-20 02:31:32 -0700

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -2,65 +2,32 @@ document.addEventListener('DOMContentLoaded', function () {
   // Select the container where you want to append the blog posts
   const blogContainer = document.querySelector('.blog-box-container');
 
-  // Fetch the list of blog posts from the server
+  // Fetch the list of blog post HTML files from the server
   fetchBlogPosts();
 
   async function fetchBlogPosts() {
     try {
-      const response = await fetch('./doc-posts/posts.html'); // Assuming you have a JSON file containing the list of posts
-      const postsData = await response.json();
+      // Assuming you have a certain number of posts, let's say 10
+      const numberOfPosts = 10;
 
-      // Iterate through the posts and append them to the blog container
-      postsData.forEach(post => {
-        const postHTML = createPostHTML(post);
+      // Iterate through the post numbers
+      for (let i = 1; i <= numberOfPosts; i++) {
+        // Construct the path for each post HTML file
+        const postFilePath = `./doc-posts/post${i}.html`;
+
+        // Fetch the HTML content of the post
+        const response = await fetch(postFilePath);
+        const postHTML = await response.text();
+
+        // Append the post HTML to the blog container
         blogContainer.insertAdjacentHTML('beforeend', postHTML);
-      });
+      }
 
       // Add event listeners to the post links after they are added to the DOM
       addPostLinkListeners();
     } catch (error) {
       console.error('Error fetching blog posts:', error);
     }
-  }
-
-  function createPostHTML(post) {
-    // Create HTML for each blog post with a link to the individual post page
-    const postHTML = `
-      <div class="blog-box">
-        <div class="blog-box-img">
-          <img alt="blog" src="${post.image}">
-          <a href="${getPostPageURL(post)}" class="blog-img-link">
-            <i class="fa-solid fa-arrow-up-right-from-square"></i>
-          </a>
-        </div>
-        <div class="blog-box-text">
-          <strong>${post.title}</strong>
-          <a href="${getPostPageURL(post)}" class="post-link">${post.summary}</a>
-          <p>${post.content}</p>
-          <div class="blog-author">
-            <div class="blog-author-img">
-              <img alt="" src="${post.authorImage}">
-            </div>
-            <div class="blog-author-text">
-              <strong>${post.author}</strong>
-              <span>${post.date}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-
-    return postHTML;
-  }
-
-  function getPostPageURL(post) {
-    // Generate the URL for the individual post page
-    return `./doc-posts/${getPostFileName(post)}.html`;
-  }
-
-  function getPostFileName(post) {
-    // Convert the post title to a valid file name
-    return post.title.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
   }
 
   function addPostLinkListeners() {
@@ -82,3 +49,4 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+
