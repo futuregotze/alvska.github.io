2d060180b771f7219876d321b2e605d9f914259c
futuregotze <33991663+futuregotze@users.noreply.github.com>
2023-12-14 09:46:10 -0700

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,51 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Select the container where you want to append the blog posts
-  const blogContainer = document.querySelector('.blog-box-container');
+// blog.js
 
-  // Fetch the list of blog post HTML files from the server
+document.addEventListener('DOMContentLoaded', function () {
+  const blogContainer = document.querySelector('.blog-box-container');
+  const loadMoreButton = document.getElementById('load-more-button');
+  const loadMoreContainer = document.getElementById('load-more-container');
+  const postsPerPage = 3;
+  let currentPage = 1;
+
+  loadMoreButton.addEventListener('click', function () {
+    currentPage++;
+    fetchBlogPosts();
+  });
+
   fetchBlogPosts();
 
   async function fetchBlogPosts() {
     try {
-      // Assuming you have a certain number of posts, let's say 10
-      const numberOfPosts = 3; // Display only 3 posts at a time
+      const startIndex = (currentPage - 1) * postsPerPage + 1;
+      const endIndex = startIndex + postsPerPage - 1;
 
-      // Iterate through the post numbers
-      for (let i = 1; i <= numberOfPosts; i++) {
-        // Construct the path for each post HTML file
+      for (let i = startIndex; i <= endIndex; i++) {
         const postFilePath = `./doc-posts/post${i}.html`;
-
-        // Fetch the HTML content of the post
         const response = await fetch(postFilePath);
         const postHTML = await response.text();
-
-        // Append the post HTML to the blog container
         blogContainer.insertAdjacentHTML('beforeend', postHTML);
       }
 
-      // Add event listeners to the post links after they are added to the DOM
-      addPostLinkListeners();
+      if (!hasMorePosts(currentPage)) {
+        // If there are no more posts, hide the load more container
+        loadMoreContainer.style.display = 'none';
+      }
     } catch (error) {
       console.error('Error fetching blog posts:', error);
     }
   }
 
-  function addPostLinkListeners() {
-    // Select all blog boxes
-    const blogBoxes = document.querySelectorAll('.blog-box');
-
-    // Add a click event listener to each blog box
-    blogBoxes.forEach((box, index) => {
-      const postLink = box.querySelector('.blog-img-link');
-
-      // Add a click event listener to each blog image link
-      postLink.addEventListener('click', function (event) {
-        // Prevent the default link behavior to avoid navigating to the URL
-        event.preventDefault();
-
-        // Redirect the user to the individual post page
-        window.location.href = `./doc-posts/post${index + 1}.html`;
-      });
-    });
+  function hasMorePosts(currentPage) {
+    const nextPostExists = document.querySelector(`.blog-box[data-post-index="${currentPage * postsPerPage + 1}"]`);
+    return !!nextPostExists;
   }
 });
