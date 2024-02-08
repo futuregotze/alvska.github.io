4aff04c468e0f8d1e943c09d035d083a4ddd59a6
futuregotze <33991663+futuregotze@users.noreply.github.com>
2024-02-08 16:55:32 -0700

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -1,10 +1,15 @@
 // tree.js
-document.addEventListener('DOMContentLoaded', function () {
-  var treeItems = document.querySelectorAll('.tree details summary');
+document.addEventListener('DOMContentLoaded', function() {
+  var detailsElements = document.querySelectorAll('.tree details');
 
-  treeItems.forEach(function (item) {
-    item.addEventListener('click', function () {
-      item.classList.toggle('open');
+  detailsElements.forEach(function(detailsElement) {
+    detailsElement.addEventListener('toggle', function() {
+      var summaryElement = detailsElement.querySelector('summary');
+      if (detailsElement.hasAttribute('open')) {
+        summaryElement.innerHTML = '<i class="fa-duotone fa-folder-open" style="--fa-primary-color: #606060; --fa-secondary-color: #232323;"></i> ' + summaryElement.textContent;
+      } else {
+        summaryElement.innerHTML = '<i class="fa-sharp fa-solid fa-folder-closed" style="color: #606060;"></i> ' + summaryElement.textContent;
+      }
     });
   });
 });
