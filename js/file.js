a960a34a08f1bb55ced5e7c7b7bc86913baaf5d7
futuregotze <33991663+futuregotze@users.noreply.github.com>
2024-02-08 18:34:30 -0700

    Update file.js

--- js/file.js
+++ js/file.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var fileLinks = document.querySelectorAll('.file-link');
-
-  fileLinks.forEach(function(link) {
-    link.addEventListener('click', function() {
-      var fileId = link.dataset.file;
+  document.addEventListener('click', function(event) {
+    var target = event.target;
+    if (target.classList.contains('file-link')) {
+      event.preventDefault(); // Prevent default link behavior
+      var fileId = target.dataset.file;
       window.open(`https://drive.google.com/file/d/${fileId}/view`, '_blank');
-    });
+    }
   });
 });
