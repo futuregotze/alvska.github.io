f56f26abd1b357e1c3424d5c90a34ad102f52962
futuregotze <33991663+futuregotze@users.noreply.github.com>
2024-02-08 17:04:07 -0700

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     detailsElement.addEventListener('toggle', function() {
       var summaryElement = detailsElement.querySelector('summary');
       if (detailsElement.hasAttribute('open')) {
-        summaryElement.innerHTML = '<i class="fa-duotone fa-folder-open" style="--fa-primary-color: #606060; --fa-secondary-color: #232323;"></i> ' + summaryElement.textContent;
+        summaryElement.innerHTML = '<i class="fa-sharp fa-solid fa-folder-open" style="color: #606060;"></i> ' + summaryElement.textContent;
       } else {
         summaryElement.innerHTML = '<i class="fa-sharp fa-solid fa-folder-closed" style="color: #606060;"></i> ' + summaryElement.textContent;
       }
