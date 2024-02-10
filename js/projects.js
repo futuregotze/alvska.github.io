18cf3aa311165c7ebe79a9cd2bdeda4302619ebc
Jose Fernandez <33991663+futuregotze@users.noreply.github.com>
2024-02-09 21:46:22 -0700

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,13 @@
 // JavaScript file for handling button click event and redirection
 
-document.getElementById('my-work-link').addEventListener('click', function() {
-    // You can add additional logic here if needed
-    // For now, simply redirect to ./tree.html
-    window.location.href = './tree.html';
+document.addEventListener('DOMContentLoaded', function() {
+    // Add event listener after the DOM is fully loaded
+    document.getElementById('my-work-link').addEventListener('click', function(event) {
+        // Prevent the default behavior of the anchor tag
+        event.preventDefault();
+
+        // You can add additional logic here if needed
+        // For now, simply redirect to ./tree.html
+        window.location.href = './tree.html';
+    });
 });
