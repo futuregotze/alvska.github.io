484eba15502023f60a9af490d03f232898d4a2c5
futuregotze <33991663+futuregotze@users.noreply.github.com>
2024-02-09 20:41:47 -0700

    Create projects.js

---
+++ js/projects.js
@@ -0,0 +1,7 @@
+// JavaScript file for handling button click event and redirection
+
+document.getElementById('my-work-link').addEventListener('click', function() {
+    // You can add additional logic here if needed
+    // For now, simply redirect to ./tree.html
+    window.location.href = './tree.html';
+});
