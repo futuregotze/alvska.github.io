b7a297fc3c3a89e7bc02b98471b814436b1f5998
futuregotze <33991663+futuregotze@users.noreply.github.com>
2024-02-09 19:38:40 -0700

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -1,34 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
   var detailsElements = document.querySelectorAll('.tree details');
 
-  // Search functionality
-  var searchInput = document.getElementById('tree-search');
-  searchInput.addEventListener('input', function() {
-    var searchTerm = searchInput.value.toLowerCase();
-
-    detailsElements.forEach(function(detailsElement) {
-      var summaryElement = detailsElement.querySelector('summary');
-      var summaryText = summaryElement.textContent.toLowerCase();
-
-      if (summaryText.includes(searchTerm)) {
-        detailsElement.style.display = 'block';
-      } else {
-        detailsElement.style.display = 'none';
-      }
-    });
-  });
-
   detailsElements.forEach(function(detailsElement) {
     detailsElement.addEventListener('toggle', function() {
       var summaryElement = detailsElement.querySelector('summary');
       var iconElement = summaryElement.querySelector('i');
 
       if (detailsElement.hasAttribute('open')) {
-        iconElement.classList.remove('fa-folder-closed');
-        iconElement.classList.add('fa-folder-open');
+        iconElement.className = 'fa-sharp fa-solid fa-folder-open'; // Change the class to the open folder icon
       } else {
-        iconElement.classList.remove('fa-folder-open');
-        iconElement.classList.add('fa-folder-closed');
+        iconElement.className = 'fa-sharp fa-solid fa-folder-closed'; // Change the class to the closed folder icon
       }
     });
   });
